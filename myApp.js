325c0461dd539bd0b9a1b7848e4cc430e4d4f095
87mad230 <ghost-man230@outlook.com>
2021-08-29 13:38:05 -0700

    :)

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(process.env.MONGO_URI);
+mongoose.connect('mongodb+srv://test:test@cluster0.nxd1a.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let personSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: Array
+});
+
+let Person = mongoose.model('Person',personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
